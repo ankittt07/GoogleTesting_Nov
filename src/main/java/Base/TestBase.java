1e8a00dfe816feafea023ae26151bb125e14894b
ankittt07 <ankitpandey2307@gmail.com>
2022-10-14 22:09:23 +0530

    |First Commit|

---
+++ src/main/java/Base/TestBase.java
@@ -0,0 +1,11 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Properties;
+
+public class TestBase {
+    public static WebDriver driver=null;
+    public static Properties properties;
+
+}
